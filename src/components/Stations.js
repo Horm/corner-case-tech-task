1b8b70d179adddf18308ac1cba2dcd513a1439cb
Horm <horak@horm.cz>
2019-03-18 09:51:33 +0100

    Add state structure and empty components

---
+++ src/components/Stations.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Stations = ({}) => <div></div>;
+
+export default Stations
---
+++ src/components/Toolbar.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Toolbar = ({}) => <div></div>;
+
+export default Toolbar
