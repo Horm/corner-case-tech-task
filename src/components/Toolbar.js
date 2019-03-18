73efdffdb7689cb394e62130d5c6837e2a4c42da
Horm <horak@horm.cz>
2019-03-18 10:04:22 +0100

    Add toolbar HTML structure

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,5 +1,21 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-const Toolbar = ({}) => <div></div>;
+const Toolbar = ({handleBackClick, handleQuitClick}) =>
+    <div>
+      <button onClick={handleBackClick}>Back</button>
+      <h1>Stations</h1>
+      <button onClick={handleQuitClick}>Quit</button>
+    </div>;
+
+Toolbar.propTypes = {
+  handleBackClick: PropTypes.func.isRequired,
+  handleQuitClick: PropTypes.func.isRequired,
+};
+
+Toolbar.defaultProps = {
+  handleBackClick: () => {},
+  handleQuitClick: () => {},
+};
 
 export default Toolbar
